eeba64263ef68a9a769c0ec5787dd08893e79558
Vedant275 <vedant275sharma@gmail.com>
2025-10-08 16:25:10 +0530

    Create BubbleSort.java

---
+++ Data Structures/Sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main {
+
+    static int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    static boolean checkSort(int[] numbers) {
+        // function to check if the array is already sorted in ascending order
+        for (int pos = 0; pos < numbers.length - 1; pos++) {
+            if (numbers[pos] > numbers[pos + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+        public static void main (String[]args) {
+            // if array is already sorted, print it directly
+            if (checkSort(numbers) == true){
+                for (int j = 0; j < numbers.length; j++) {
+                    System.out.println(numbers[j]);
+                }
+                return;
+            } else {
+                int temp;
+                // bubble sort to sort array in ascending order
+                for (int pos = 0; pos < numbers.length - 1; pos++) {
+                    for (int i = 0; i < numbers.length - pos - 1; i++) {
+                        if (numbers[i] > numbers[i + 1]) {
+                            temp = numbers[i];
+                            numbers[i] = numbers[i + 1];
+                            numbers[i + 1] = temp;
+                        }
+                    }
+                }
+                for (int j = 0; j < numbers.length; j++) {
+                    System.out.println(numbers[j]);
+                }
+            }
+        }
+    }
+
+
